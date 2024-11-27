db340d97e3c6257e96c3c436ea0b40d16a011c3f
xtaxadvisor <gdoffice@mail.com>
2024-11-27 00:33:02 +0000

    Updated React components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -24,7 +24,8 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+        git diff
+        </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
